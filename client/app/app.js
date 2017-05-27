77edbdcede184a37d512c4ebf1a9c15a72445ea4
bratc <bramasterweb@gmail.com>
2017-05-27 17:23:50 -0500

    Inicia proyecto videoClub

---
+++ client/app/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('videoClubApp', [
+	'videoClubApp.constants',
+	 'ngCookies', 
+	 'ngResource', 
+	 'ngSanitize',
+     'ui.router', 
+     'ui.bootstrap'
+  ])
+.constant("API","http://localhost:25956/adsi2017_completo")
+  .config(function($urlRouterProvider, $locationProvider) {
+    $urlRouterProvider.otherwise('/');
+
+    $locationProvider.html5Mode(true);
+  });
